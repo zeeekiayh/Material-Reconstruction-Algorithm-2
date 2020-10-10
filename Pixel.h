c2cfbc452791284d2890396f9bf93f3442c687f2
zeeekiayh <60297297+zeeekiayh@users.noreply.github.com>
2020-10-10 15:55:27 -0600

    Update Pixel.h

--- Pixel.h
+++ Pixel.h
@@ -3,56 +3,54 @@
 #ifndef PIXEL_H
 #define PIXEL_H
 
-
 class Pixel 
 {
 
-	public:
+    public:
+//  ======================
+    Pixel() // Constructor
+    {
+        typ = 0;
+        bestMatch = NULL;
+    votes = 0;
+    }
 
-//      =======================
-	Pixel() // Constructor
-	{
-		typ = 0;
-		bestMatch = NULL;
-		votes = 0;
-	}
+    ~Pixel() {} // Destructor
+//  =========================
 
-	~Pixel() {} // Destructor
-//      ======================
+//  ===================
+//  Pixel Class Methods
+//  ===================
+    void SetType(int typ);
+    void AddVote(int);
 
-//      ===================
-//      Pixel Class Methods
-//      ===================
-	void SetType(int typ);
-	void AddVote(int);
+    void SetBestMatch(Pixel* bestMatch) { this->bestMatch = bestMatch;}
+    int GetType() const { return typ; }
+    int GetVotes() { return votes; }
+//  ================================
 
-	void SetBestMatch(Pixel* bestMatch) { this->bestMatch = bestMatch;}
-	int GetType() const { return typ; }
-	int GetVotes() { return votes; }
-//      ===============
+    //prints the pixel
+    friend std::ostream& operator << (std::ostream& out, Pixel rhs)
+    {
+        out << rhs.typ << " ";
+        return out;
+    }
 
-	//prints the pixel
-	friend std::ostream& operator << (std::ostream& out, Pixel rhs)
-	{
-		out << rhs.typ << " ";
-		return out;
-	}
+    private:
 
-	private:
+    //this describes the 'color' of the pixel
+    int typ;
+	
+    //this points to the Pixel who's neighborhood
+    //  best matches this Pixel's neighborhood
+    Pixel* bestMatch;
 
-        //this describes the 'color' of the pixel
-	int typ;
+    //during the algorithm, the votes on each
+    //  Pixel are summed; this will tell us, at
+    //  the end of each iteration, if we need to
+    //  change it or not
+    int votes;
 		
-	//this points to the Pixel who's neighborhood
-	//  best matches this Pixel's neighborhood
-	Pixel* bestMatch;
-
-	//during the algorithm, the votes on each
-	//  Pixel are summed; this will tell us, at
-	//  the end of each iteration, if we need to
-	//  change it or not
-	int votes;
-			
 };//end Pixel()
-//  ===============
+
 #endif
